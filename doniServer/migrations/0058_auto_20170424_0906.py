6c8a1797093002f6af33200915e28c18f3f6374a
immadimtiaz <immadimtiaz@gmail.com>
2017-04-24 14:09:37 +0500

    changes

--- doniServer/migrations/0058_auto_20170424_0906.py
+++ doniServer/migrations/0058_auto_20170424_0906.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('doniServer', '0057_auto_20170424_0847'),
+        ('doniServer', '0056_pricemetric_kgs'),
     ]
 
     operations = [
