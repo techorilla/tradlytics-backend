2ba0146b01c95e0448ff2a664adb14859d307867
immadimtiaz <immadimtiaz@gmail.com>
2017-04-30 16:32:53 +0500

    changes

--- doniGroup/settings/constants.py
+++ doniGroup/settings/constants.py
@@ -1,3 +1,7 @@
 SESSION_COOKIE_AGE = 60*60*2
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'immadimtiaz@gmail.com'
 
